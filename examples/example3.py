9ebc820b14107be2ef63f8df6a80b9505557906a
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-02-20 19:48:51 +0000

    wip

---
+++ examples/example3.py
@@ -0,0 +1,34 @@
+"""Example 1"""
+
+import asyncio
+
+from jetblack_ksqldb import KsqlDbClient
+
+
+async def main() -> None:
+    """Entrypoint"""
+
+    ksqldb = KsqlDbClient()
+
+    props = {
+        "auto.offset.reset": "earliest"
+    }
+
+    sql = """
+-- Mountain View lat, long: 37.4133, -122.1162
+SELECT
+    *
+FROM
+    riderLocations
+WHERE
+    GEO_DISTANCE(latitude, longitude, 37.4133, -122.1162) <= 5
+EMIT CHANGES;
+"""
+    async for row in ksqldb.query_stream(sql, properties=props):
+        print("run_query", row)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
