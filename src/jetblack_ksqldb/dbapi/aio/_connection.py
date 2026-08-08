c8ace85d122bbab8f130a1724f6b3a0ab5ebdd7e
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-08-08 09:13:39 +0100

    Added an async version

---
+++ src/jetblack_ksqldb/dbapi/aio/_connection.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from typing import (
+    Literal,
+    Self,
+)
+
+from httpx import (
+    AsyncClient,
+    BasicAuth
+)
+
+from jetblack_ksqldb.dbapi._paramstyles import ParamStyle
+
+from ._cursor import Cursor
+from .._exceptions import Error
+from .._ksql_inspector import KsqlInspector
+from .._types import FormatConfig
+
+
+type QueryType = Literal['print', 'select']
+
+CONTENT_TYPE_JSON = "application/vnd.ksql.v1+json"
+CONTENT_TYPE_DELIMITED = "application/vnd.ksqlapi.delimited.v1"
+
+
+class Connection:
+
+    def __init__(
+            self,
+            client: AsyncClient,
+            binding_config: FormatConfig,
+            paramstyle: ParamStyle = "qmark"
+    ) -> None:
+        self._client = client
+        self._binding_config = binding_config
+        self._inspector = KsqlInspector()
+        self._paramstyle = paramstyle
+
+    @classmethod
+    def connect(
+            cls,
+            url: str = "http://localhost:8088",
+            api_key: str | None = None,
+            api_secret: str | None = None,
+            binding_config: FormatConfig | None = None,
+            paramstyle: ParamStyle = "qmark"
+    ) -> Self:
+        auth = (
+            BasicAuth(api_key, api_secret)
+            if api_key and api_secret else
+            None
+        )
+        client = AsyncClient(base_url=url, auth=auth, http1=False, http2=True)
+        return cls(client, binding_config or FormatConfig(), paramstyle=paramstyle)
+
+    def cursor(self) -> Cursor:
+        if self._client.is_closed:
+            raise Error("Connection is closed")
+
+        return Cursor(self._client, self._binding_config, self._inspector, paramstyle=self._paramstyle)
+
+    async def close(self) -> None:
+        await self._client.aclose()
+
+    def commit(self) -> None:
+        raise Error("ksql does not support transactions")
+
+    def rollback(self) -> None:
+        raise Error("ksql does not support transactions")
+
+
+connect = Connection.connect
---
+++ src/jetblack_ksqldb/dbapi/aio/_cursor.py
@@ -0,0 +1,277 @@
+from __future__ import annotations
+
+import json
+from typing import (
+    Any,
+    AsyncIterator,
+    Literal,
+    Mapping,
+    Sequence,
+    cast
+)
+
+import httpx
+from httpx import (
+    AsyncClient,
+    HTTPStatusError,
+    Timeout,
+    USE_CLIENT_DEFAULT,
+)
+
+from jetblack_ksqldb.dbapi._exceptions import NotSupportedError
+from jetblack_ksqldb.dbapi._paramstyles import ParamStyle
+
+from ..._types import QueryMetaData, create_ksql_error
+
+from .._binding import bind_parameters
+from .._description import Description
+from .._exceptions import Error, NotSupportedError
+from .._ksql_inspector import KsqlInspector
+from .._statement_transformer import StatementStyle, StatementType
+from .._types import FormatConfig
+
+
+type QueryType = Literal['print', 'select']
+
+CONTENT_TYPE_JSON = "application/vnd.ksql.v1+json"
+CONTENT_TYPE_DELIMITED = "application/vnd.ksqlapi.delimited.v1"
+
+
+class Cursor:
+
+    def __init__(
+            self,
+            client: AsyncClient,
+            binding_config: FormatConfig,
+            inspector: KsqlInspector,
+            paramstyle: ParamStyle,
+            close_timeout: float = 1.0,
+    ) -> None:
+        self._client = client
+        self._binding_config = binding_config
+        self._inspector = inspector
+        self._paramstyle = paramstyle
+        self._query_id: str | None = None
+        self._iter: AsyncIterator[Sequence[Any]] | None = None
+        self._description: list[Description] | None = None
+        self._close_timeout = close_timeout
+
+        self.arraysize: int = 1
+
+    @property
+    def description(self) -> list[Description] | None:
+        return self._description
+
+    @property
+    def rowcount(self) -> int:
+        # TODO: There can only be a rowcount for "pull" queries. We could detect
+        # this in the parser. Then we would need to fetch all the rows and wrap
+        # the result iterator.
+        return -1
+
+    def callproc(self, procname: str, parameters: Sequence[Any] | None = None) -> None:
+        raise NotSupportedError("ksql does not support stored procedures")
+
+    async def close(self) -> None:
+        if self._query_id is None:
+            return
+
+        headers = {
+            "content-type": CONTENT_TYPE_JSON
+        }
+
+        body = {
+            "queryId": self._query_id
+        }
+
+        response = await self._client.post(
+            "/close-query",
+            headers=headers,
+            json=body,
+            timeout=self._close_timeout
+        )
+        if not response.is_success:
+            raise Error("Failed to close query")
+
+    async def execute(
+            self,
+            query: str,
+            params: Sequence[Any] | Mapping[str, Any] | None = None
+    ) -> None:
+        if params:
+            bound_query = bind_parameters(
+                query,
+                params,
+                self._paramstyle,
+                self._binding_config
+            )
+        else:
+            bound_query = query
+
+        statement_tuple = self._inspector.find_statement_type(bound_query)
+
+        match statement_tuple:
+
+            case (StatementStyle.COMMAND, _):
+                await self._ksql(bound_query)
+            case (StatementStyle.QUERY, StatementType.SELECT):
+                await self._query_stream(bound_query, 'select')
+            case (StatementStyle.QUERY, StatementType.PRINT):
+                await self._query_stream(bound_query, 'print')
+
+    async def executemany(
+            self,
+            query: str,
+            params: Sequence[Sequence[Any]] | Sequence[Mapping[str, Any]]
+    ) -> None:
+        if len(params) == 0:
+            raise ValueError("Must have params")
+
+        bound_queries = [
+            bind_parameters(
+                query,
+                args,
+                self._paramstyle,
+                self._binding_config
+            )
+            for args in params
+        ]
+
+        (statement_style, _) = self._inspector.find_statement_type(
+            bound_queries[0])
+        if statement_style != StatementStyle.COMMAND:
+            raise ValueError("Expected a command")
+
+        ksql = "".join(bound_queries)
+        await self._ksql(ksql)
+
+    async def _ksql(
+            self,
+            ksql: str,
+            *,
+            streams_properties: Mapping[str, str] | None = None,
+            session_variables: Mapping[str, str] | None = None,
+            command_sequence_number: int | None = None,
+            timeout: Timeout | None = None
+    ) -> list[Any]:
+        headers = {
+            "content-type": CONTENT_TYPE_JSON
+        }
+
+        body: dict[str, Any] = {
+            "ksql": ksql,
+            "streamsProperties": streams_properties or {},
+        }
+        if session_variables is not None:
+            body['sessionVariables'] = session_variables
+        if command_sequence_number is not None:
+            body['commandSequenceNumber'] = command_sequence_number
+
+        response = await self._client.post(
+            "/ksql",
+            headers=headers,
+            json=body,
+            timeout=timeout or USE_CLIENT_DEFAULT
+        )
+        if response.is_success:
+            return response.json()
+
+        if response.status_code == httpx.codes.BAD_REQUEST:
+            error = create_ksql_error(response.json())
+            raise error
+
+        raise HTTPStatusError(
+            f"{response.status_code}: {response.reason_phrase}",
+            request=response.request,
+            response=response
+        )
+
+    async def _query_stream(
+            self,
+            sql: str,
+            query_type: QueryType,
+            *,
+            timeout: float = 1.0,
+            properties: dict[str, Any] | None = None
+    ) -> None:
+        headers = {
+            "content-type": (
+                CONTENT_TYPE_JSON
+                if query_type == 'select' else
+                CONTENT_TYPE_DELIMITED
+            )
+        }
+
+        body: dict[str, Any] = {
+            "sql": sql,
+            "properties": properties or {},
+        }
+
+        request = self._client.build_request(
+            "POST",
+            "/query-stream",
+            headers=headers,
+            json=body,
+            timeout=Timeout(timeout, read=None)
+        )
+        response = await self._client.send(
+            request=request,
+            stream=True
+        )
+
+        response.raise_for_status()
+        if query_type == 'select':
+            line_iter = response.aiter_lines()
+            meta_data = cast(QueryMetaData, json.loads(await anext(line_iter)))
+            self._description = Description.create_all(meta_data)
+            self._query_id = meta_data['queryId']
+            self._iter = (self._to_row(line) async for line in line_iter)
+        else:
+            self._iter = response.aiter_lines()
+            self._description = None
+
+    def _to_row(self, line: str) -> Sequence[Any]:
+        assert self._iter is not None
+        assert self._description is not None
+        row = json.loads(
+            line,
+            parse_float=lambda x: x,
+            parse_int=lambda x: x
+        )
+        return [
+            description.type_code._from_str(value, self._binding_config)
+            for value, description in zip(row, self._description)
+        ]
+
+    async def fetchone(self) -> Sequence[Any]:
+        if self._iter is None:
+            raise Error("No results available")
+
+        return await anext(self._iter)
+
+    async def fetchmany(self, size: int | None = None) -> Sequence[Sequence[Any]]:
+        if self._iter is None:
+            raise Error("No results available")
+
+        if size is None:
+            size = self.arraysize
+
+        return [await anext(self._iter) for _ in range(size)]
+
+    async def fetchall(self) -> Sequence[Sequence[Any]]:
+        if self._iter is None:
+            raise Error("No results available")
+
+        return [row async for row in self._iter]
+
+    def nextset(self) -> bool | None:
+        return None
+
+    def setinputsizes(self, sizes: Sequence[Any]) -> None:
+        pass
+
+    async def __aiter__(self) -> AsyncIterator[Sequence[Any]]:
+        if self._iter is None:
+            raise Error("No results available")
+
+        return self._iter
